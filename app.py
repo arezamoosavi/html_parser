b57c9fc9dee897efd8865d01e9bf84f9a17e81ba
alireza <sdamoosavi@gmail.com>
2020-04-03 23:06:00 +0430

    naming is reformed!

--- app.py
+++ app.py
@@ -7,41 +7,37 @@ parse_result = {}
 
 with zipfile.ZipFile("lot-parser.zip", "r") as zip:
     html_file_path = [item.filename for item in zip.filelist if item.filename.endswith('.html')]
-    # print(len(html_file_path))
     for file in html_file_path:
         if file.startswith('__MACOSX'):
             continue
-        print('\n','*'*5,file,'*'*5,'\n')
         htmlfile = zip.read(file).decode("utf-8")
-        # bs1 = BeautifulSoup(htmlfile, "html.parser")
-        bs2 = BeautifulSoup(htmlfile, "html5lib")
+        bs_htmlfile = BeautifulSoup(htmlfile, "html5lib")
         
-        p1 = bs2.find('div')
-        p11 = [item.strip().replace('  ', ' ') for item in p1.contents]
-        
-        p21 = list(map(lambda x:x.strip(), p11[0].split("\n")))
-        Artist = p21[0]
-        ArtName = p21[1]
-        About = ' '.join(p21[2:])
+        div0_htmlfile = bs_htmlfile.find('div')
+        extracted_data = list(map(lambda x:x.strip(), div0_htmlfile.contents[0].split("\n")))
+
+        Artist = extracted_data[0]
+        ArtName = extracted_data[1]
+        About = ' '.join(extracted_data[2:])
         
         print('Artist: ', Artist)
         print('ArtName: ', ArtName)
         print('About: ', About)
         
-        s1 = bs2.find('span')
-        if s1:
-            s2 = s1.find_next()
-            Price = ' '.join([s1.contents[0], s2.contents[0]])
+        span0_htmlfile = bs_htmlfile.find('span')
+        if span0_htmlfile:
+            span1_htmlfile = span0_htmlfile.find_next()
+            Price = ' '.join([span0_htmlfile.contents[0], span1_htmlfile.contents[0]])
             print('Price: ',Price)
         else:
-            s1 = p1.find_next()
-            print(s1.contents)
+            div1_htmlfile = div0_htmlfile.find_next()
+            print(div1_htmlfile.contents)
 
-            s2 = s1.find_next()
-            print(s2.contents)
+            div2_htmlfile = div1_htmlfile.find_next()
+            print(div2_htmlfile.contents)
 
-            # Price = ' '.join([s2.contents[0], s1.contents[0]])
-            Price = s2.contents[0]
+            # Price = ' '.join([span1_htmlfile.contents[0], span0_htmlfile.contents[0]])
+            Price = div2_htmlfile.contents[0]
             print('Price: ',Price)
             # break
             continue
